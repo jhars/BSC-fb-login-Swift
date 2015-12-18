3b7d79e0560fa3479f614f1746041e73fe20bfaf
jhars <jjharlan06@gmail.com>
2015-12-17 21:43:11 -0800

    added data-flow notes, and data-btn works

--- BabySitters Club/fbLoginViewController.swift
+++ BabySitters Club/fbLoginViewController.swift
@@ -15,6 +15,26 @@ class fbLoginViewController: UIViewController {
     @IBAction func userFbData(sender: UIButton) {
         print("data here")
         self.returnUserData()
+        //POST Data to Internal API after this
+        // => ID + Name (from fb) -- new User
+        // => package w/ Zip-Code (from user-input)
+        // (initial) UserObj == [{}]
+//        var UserObject = [
+//            {"name" : "Jon Harlan" },
+//            {"id" : "10205360690837256"},
+//            {"zip" : "94117"}
+//            ]
+        // => hit URL... sitterbook.com/user/:id
+        // => Simoultaneously send Data to Server
+        
+        // ON SERVER
+        // Server Imports User-Social-Context (friends)
+        // Imports Internal API info (id, name, zip)
+        //     Packages:
+        // client-object (id, zip)
+        //       WITH
+        // server-object (friends)
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +46,7 @@ class fbLoginViewController: UIViewController {
     
     func returnUserData()
     {
-        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "10153702169405690?fields=context.fields%28all_mutual_friends.limit%28100%29%29", parameters: nil)
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me/friends", parameters: nil)
         graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
             
             if ((error) != nil)
