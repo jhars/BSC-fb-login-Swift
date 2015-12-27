64b02a078750e58af16d8f9fd2d121239a74fc18
jhars <jjharlan06@gmail.com>
2015-12-27 01:50:38 -0800

    image loads, firebase data imported, aware of fbUserID

--- BabySitters Club/ReadSitterDataTableViewController.swift
+++ BabySitters Club/ReadSitterDataTableViewController.swift
@@ -5,7 +5,8 @@
 //  Created by Jon Harlan on 12/24/15.
 //  Copyright © 2015 Jon Harlan. All rights reserved.
 //
-
+import Alamofire
+import AlamofireImage
 import UIKit
 
 //protocol ReadSitterDataTVCDelegate {
@@ -13,78 +14,71 @@ import UIKit
 //}
 class ReadSitterDataTableViewController: UITableViewController {
 
-    @IBOutlet weak var testImage: UIImageView!
+//    var urlBaseRef = "h ttps://sitterbookapi.firebaseio.com/users/10205360690837256/fbfriends/first-degree/"
+    
+
+//    let testRef02 = Firebase(url:urlBaseRef + "23")
+//    let testRef03 = Firebase(url:urlBaseRef + "27")
+//    let testRef04 = Firebase(url:urlBaseRef + "33")
+//    let testRef05 = Firebase(url:urlBaseRef + "34")
+//    let testRef06 = Firebase(url:urlBaseRef + "35")
+//    let testRef07 = Firebase(url:urlBaseRef + "39")
+//    let testRef08 = Firebase(url:urlBaseRef + "40")
+    
+    
+    
+
     
     //---------------------- //
     // URL -> IMAGE:LOADER  //
+    @IBOutlet weak var testImage: UIImageView!
     //----------------------//
+    let AlamoRef = Alamofire.request(.GET, "https://scontent.xx.fbcdn.net/hprofile-xpt1/v/t1.0-1/p50x50/12190919_10102581058954074_1908735207991004359_n.jpg?oh=2e458b46499f38c47d50a324a30c0ad8&oe=5716104D")
     
-    //Key Variables:
-    
-    //=A=>    imageView: UIImageView! (@IBOutlet)
-    //        ->Needs to be attached to aUIImageView on the Main.StoryBoard
-    
-    //=B=>    dataUrl = any Url for Single Image
-    
-    //=C=>    'imageView' - Read Lines 60-64
-    
-    //=================== DELEGATABLE ========================//
-    // - -  - - - - - - - UI ImagView - - - - - -  - - - - - //
-    // MARK: A
-//    @IBOutlet weak var imageView: UIImageView!
-    // MARK: B
-    let dataUrl = "https://scontent.xx.fbcdn.net/hprofile-xpl1/v/t1.0-1/p50x50/17016_10205195417825534_5350715931583113657_n.jpg?oh=4d4de150626c1aecdf93ff6e7f6c0d9c&oe=57091762"
-    //"h ttps://lh5.ggpht.com/tq3WqEUxtRyBn-d_0t3j6WKNHuJDrmLq-FE3GAYrsAMQFIaS7FIgRLfzzql2SvfvLqto=w300"
-    
-    //=================== DELEGATABLE ========================//
-    // - - - - - - - - - - - - - - - - - - - - - - - - - \\
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - \\
-    //======================== Custom ViewController Code Below ===================//
+//    var sitterArrayUrl:Array = [String]()
+
     //   INITIALIZER (Action)
     @IBAction func refreshSitters(sender: AnyObject) {
 
         //=================== DELEGATABLE ========================//
-        requestImage(dataUrl) { (image) -> Void in
-            let myImage = image
+        AlamoRef.responseImage { response in
+            debugPrint(response)
+//            print(response.request)
+//            print(response.response)
+            debugPrint(response.result)
             
-            //Last Piece of Custom Code! => 'imageView' as @IBOutlet
-            // - -  - - - - - - - - - - - - - - - - - - - - - - - - - //
-            // MARK: C   ->    UI ImageView
-            self.testImage.image = myImage
-        }   //     UI ImageView
-        // - -  - - - - - - - - - - - - - - - - - - -  - - - - - //
-        //        replace 'imageView' with whatever you named your '@IBOutlet / UIImageView' above
+            if let image = response.result.value {
+//                print("image downloaded: \(image)")
+                self.testImage.image = image
+                print("image loaded!")
+            }
+        }
         //=================== DELEGATABLE ========================//
+            print("image Urls???")
+            getSitterData("https://sitterbookapi.firebaseio.com/users/1734329503463628/fbfriends/first-degree/0")
         
     }
+    
+
+    
+    func getSitterData(imgUrlString:String) {
+        
+        let testRef = Firebase(url:imgUrlString)
+        
+        testRef.observeEventType(.Value, withBlock: { snapshot in
+//            print(snapshot.value)
+            var userData = snapshot.value
+            print(userData)
+            
+            }, withCancelBlock: { error in
+                print(error.description)
+        })
+    }
+    
+    
 //    this could be my Array of Initial URL Strings
     var tableData = [String]()
     
-    // - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-    func requestURL(url: String, success: (NSData?) -> Void, error: ((NSError) -> Void)? = nil) {
-        let session = NSURLSession.sharedSession()
-        let url = NSURL(string: dataUrl)!
-        
-        session.dataTaskWithURL(url, completionHandler: { ( data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
-            guard let realResponse = response as? NSHTTPURLResponse where
-                realResponse.statusCode == 200 else {
-                    print("Not a 200 response")
-                    return
-            }
-            do {//--DATA--//
-                success(data)
-            }   //--DATA--//
-        }).resume()
-        //MORE SETUP OPTIONS HERE...
-    }
-    func requestImage(url: String, success: (UIImage?) -> Void) {
-        requestURL(url, success: { (data) -> Void in
-            if let d = data {
-                success(UIImage(data: d))
-            }
-        })
-    }
-    // - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
     //============= Facebook Data - User ID ================= //
     func returnUserData() {
         let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
@@ -120,16 +114,12 @@ class ReadSitterDataTableViewController: UITableViewController {
     
     //Also Need Access to Photos on Device (iPhone)
 
-    // - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  //
-    //======================== Custom ViewController Code Above ===================//
-    // - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-    //_______________________________[{END CUSTOM CODE}]___________________________//
-
 //   INITIALIZER (Action)
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var fbUserData = self.returnUserData()
+
 
     }
     override func didReceiveMemoryWarning() {
